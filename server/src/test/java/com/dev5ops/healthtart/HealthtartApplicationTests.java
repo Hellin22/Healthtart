911dfbda6bdbe04a73de024f879a3b3083759e49
백경석\seok <dno06117@naver.com>
2025-04-25 02:22:35 +0900

    ✨ 기능 추가: 전체 context를 로드하는 SpringBootTest 주석처리 (Mock 유닛 테스트만 진행하기 때문)

--- server/src/test/java/com/dev5ops/healthtart/HealthtartApplicationTests.java
+++ server/src/test/java/com/dev5ops/healthtart/HealthtartApplicationTests.java
@@ -3,11 +3,11 @@ package com.dev5ops.healthtart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HealthtartApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
